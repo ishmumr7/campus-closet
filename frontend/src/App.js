3c0f657de25abd2a09ad569ad0124c4bde9c3286
Ishmum <ishmum20000@gmail.com>
2024-05-18 13:46:58 +0800

    Remoove unnecessary import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Bounce, ToastContainer } from "react-toastify";
 import { LoginPage, SignUpPage, ActivationPage } from "./Routes";
