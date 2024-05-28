940e45b8d03b4fcc2242f2c14940389ae90161f8
Ishmum <ishmum20000@gmail.com>
2024-05-28 17:19:40 +0800

    Add categories section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./../components/Layout/Header";
-import Hero from "./../components/Home/Hero/Hero.jsx";
+import Hero from "./../components/Home/Hero/Hero";
+import Categories from "./../components/Home/Categories/Categories.jsx";
 
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1}/>
       <Hero />
+      <Categories />
     </div>
   );
 };
