7a1024b4e92e52fd6eaff5618954c0741b84cec8
Ishmum <ishmum20000@gmail.com>
2024-05-30 13:39:31 +0800

    Add the Best deals section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./../components/Layout/Header";
 import Hero from "./../components/Home/Hero/Hero";
-import Categories from "./../components/Home/Categories/Categories.jsx";
+import Categories from "./../components/Home/Categories/Categories";
+import BestDeals from "./../components/Home/BestDeals/BestDeals.jsx";
 
 const HomePage = () => {
   return (
     <div>
-      <Header activeHeading={1}/>
+      <Header activeHeading={1} />
       <Hero />
       <Categories />
+      <BestDeals />
     </div>
   );
 };
