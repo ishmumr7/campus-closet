99e9e90cdcb7b2869fd76a6321941b47b2dca20b
Ishmum <ishmum20000@gmail.com>
2024-05-30 13:38:59 +0800

    Add product and mini details card

---
+++ frontend/src/components/Home/ProductDetailsCard/ProductDetailsCard.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import { RxCross1 } from "react-icons/rx";
+import styles from "../../../styles/styles";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineMessage,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+
+const ProductDetailsCard = ({ setOpen, data }) => {
+  const [count, setCount] = useState(1);
+  const [click, setClick] = useState(false);
+  const [select, setselect] = useState(false);
+
+  const handleMessageSubmit = () => {};
+
+  const incrementCount = () => setCount(count + 1);
+
+  const decrementCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className="bg-[#fff]">
+      {data ? (
+        <div className="fixed w-full h-screen top-0 left-0 bg-[#00000030] z-40 flex items-center justify-center">
+          <div className="w-[90%] 800px:w-[60%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
+            <RxCross1
+              size={30}
+              className="absolute right-3 top-3 z-50"
+              onClick={() => setOpen(false)}
+            />
+
+            <div className="block w-full 800px:flex">
+              <div className="w-full 800px:w-[50%]">
+                <img src={`${data.image_Url[0]?.url}`} alt="" />
+                <div className="flex">
+                  <img
+                    className=" h-[50px] w-[50px] rounded mr-2 "
+                    src={data.shop.shop_avatar.url}
+                    alt=""
+                  />
+                  <div>
+                    <h3 className={`${styles.shop_name}`}>{data.shop.name}</h3>
+                    <h5 className="pb-3 text-[15px]">
+                      ({data.shop.ratings}) Rating
+                    </h5>
+                  </div>
+                </div>
+                <div
+                  className={`${styles.button}`}
+                  style={{
+                    background: "#000",
+                    marginTop: "1rem",
+                    borderRadius: "4px",
+                    height: "2.75rem",
+                  }}
+                  onClick={handleMessageSubmit}
+                >
+                  <span className="text-[#fff] flex items-center">
+                    Send Message <AiOutlineMessage className="ml-1 " />
+                  </span>
+                </div>
+                <h5 className="text-[16px] text-[red]">
+                  {data.total_sell} Sold
+                </h5>
+              </div>
+
+              <div className="w-full 800px:w-[50%] pt-5 pl-[5px] pr-[5px] ">
+                <h1 className={`${styles.productTitle} text-[20px]`}>
+                  {data.name}
+                </h1>
+                <p>{data.description}</p>
+
+                <div className="flex pt-3">
+                  <h4 className={`${styles.productDiscountPrice}`}>
+                    {data.discount_price} RM
+                  </h4>
+                  <h3 className={styles.price}>
+                    {data.price ? data.price + " RM" : null}
+                  </h3>
+                </div>
+                <div className="flex items-center mt-12 justify-between pr-3">
+                  <div>
+                    <button
+                      className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                      onClick={decrementCount}
+                    >
+                      -
+                    </button>
+                    <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[11px]">
+                      {count}
+                    </span>
+                    <button
+                      className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                      onClick={incrementCount}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div>
+                    {click ? (
+                      <AiFillHeart
+                        size={22}
+                        className=" cursor-pointer "
+                        onClick={() => setClick(!click)}
+                        color={click ? "red" : "#333"}
+                        title="Remove from wishlist"
+                      />
+                    ) : (
+                      <AiOutlineHeart
+                        size={22}
+                        className=" cursor-pointer "
+                        onClick={() => setClick(!click)}
+                        color={click ? "red" : "#333"}
+                        title="Add to wishlist"
+                      />
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={styles.button}
+                  style={{
+                    marginTop: "1.5rem",
+                    borderRadius: "4px",
+                    height: "2.75rem",
+                    display: "flex",
+                    alignItems: "center",
+                    backgroundColor: "#000",
+                  }}
+                  // onClick={() => addToCartHandler(data._id)}
+                >
+                  <span className="text-[#fff] flex items-center">
+                    Add to cart <AiOutlineShoppingCart className="ml-1" />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default ProductDetailsCard;
