d814b4b00416ed240e98bca79ab15ec563842d0a
Ishmum <ishmum20000@gmail.com>
2024-05-30 13:46:04 +0800

    Add Featured Products section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Header from "./../components/Layout/Header";
 import Hero from "./../components/Home/Hero/Hero";
 import Categories from "./../components/Home/Categories/Categories";
-import BestDeals from "./../components/Home/BestDeals/BestDeals.jsx";
+import BestDeals from "./../components/Home/BestDeals/BestDeals";
+import FeaturedProuct from "./../components/Home/FeaturedProuct/FeaturedProuct.jsx"
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Hero />
       <Categories />
       <BestDeals />
+      <FeaturedProuct />
     </div>
   );
 };
