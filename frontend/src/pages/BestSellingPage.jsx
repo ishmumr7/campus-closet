cb8e461d5f1318d437a2543878fa4da740ff5cf2
Ishmum <ishmum20000@gmail.com>
2024-06-10 16:39:34 +0800

    Update import footer and format

--- frontend/src/pages/BestSellingPage.jsx
+++ frontend/src/pages/BestSellingPage.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../components/Layout/Header'
-import Footer from '../components/Footer/Footer'
-import { useSearchParams } from 'react-router-dom';
-import { productData } from '../static/data';
-import styles from '../styles/styles';
-import ProductCard from '../components/ProductCard/ProductCard';
+import React, { useEffect, useState } from "react";
+import Header from "../components/Layout/Header";
+import Footer from "../components/Layout/Footer";
+import { useSearchParams } from "react-router-dom";
+import { productData } from "../static/data";
+import styles from "../styles/styles";
+import ProductCard from "../components/ProductCard/ProductCard";
 
 const BestSellingPage = () => {
   const [bestData, setBestData] = useState([]);
 
   useEffect(() => {
-    const d = productData && productData.sort((a, b) => b.total_sell - a.total_sell);
+    const d =
+      productData && productData.sort((a, b) => b.total_sell - a.total_sell);
     setBestData(d);
   }, []);
-  
+
   return (
     <div>
       <Header activeHeading={2} />
@@ -22,7 +23,8 @@ const BestSellingPage = () => {
 
       <div className={styles.section}>
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-          {bestData && bestData.map((i, index) => <ProductCard data={i} key={index} />)}
+          {bestData &&
+            bestData.map((i, index) => <ProductCard data={i} key={index} />)}
         </div>
         {bestData && bestData.length === 0 ? (
           <h1 className="text-center w-full pb-[100px] text-[20px]">
@@ -32,7 +34,7 @@ const BestSellingPage = () => {
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default BestSellingPage
+export default BestSellingPage;
