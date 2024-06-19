5b100132f8fbd5ed88426c13875bd2217abcbf52
Ishmum <ishmum20000@gmail.com>
2024-06-19 13:49:08 +0800

    Add seller routes

--- backend/app.js
+++ backend/app.js
@@ -25,8 +25,10 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 // Import routes
 const user = require("./controller/user");
+const seller = require("./controller/seller");
 
 app.use("/api/v2/user", user);
+app.use("/api/v2/seller", seller);
 
 // Error handler
 app.use(ErrorHandler);
