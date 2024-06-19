101dfe207979f9da99b246f1ff382efae1f9db7f
Ishmum <ishmum20000@gmail.com>
2024-06-19 11:36:50 +0800

    Add seller signup frontend

---
+++ frontend/src/pages/SellerCreatePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SellerCreate from './../components/Shop/SellerCreate';
+
+const SellerCreatePage = () => {
+  return (
+    <div>
+      <SellerCreate />
+    </div>
+  )
+}
+
+export default SellerCreatePage;
