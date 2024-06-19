87fca6093b2d6bf7369af22994306cc430258129
Ishmum <ishmum20000@gmail.com>
2024-06-19 18:27:06 +0800

    Add seller login page

---
+++ frontend/src/pages/SellerLoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SellerLogin from '../components/Seller/SellerLogin'
+
+const SellerLoginPage = () => {
+
+  return (
+    <div>
+      <SellerLogin />
+    </div>
+  )
+}
+
+export default SellerLoginPage
