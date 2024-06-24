171ae84466a01aa4cca7d1db20654c39fe01044a
Ishmum <ishmum20000@gmail.com>
2024-06-24 20:49:28 +0800

    Add dashboard sidebar

--- frontend/src/pages/Seller/SellerDashboardPage.jsx
+++ frontend/src/pages/Seller/SellerDashboardPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import DashboardHeader from './../../components/Seller/Layout/DashboardHeader.jsx';
+import DashboardHeader from './../../components/Seller/Layout/DashboardHeader';
+import DashboardSideBar from './../../components/Seller/Layout/DashboardSideBar.jsx';
 
 const SellerDashboardPage = () => {
   return (
     <div>
       <DashboardHeader />
+      <div className="flex items-start justify-between w-full">
+            <div className="w-[80px] 800px:w-[330px]">
+              <DashboardSideBar active={1} />
+            </div>
+            {/* <DashboardHero /> */}
+          </div>
     </div>
   )
 }
