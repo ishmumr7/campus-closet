d077897b35901b2755535b4a66a485867a00164b
Ishmum <ishmum20000@gmail.com>
2024-06-24 20:16:39 +0800

    Redirect logged in seller

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,14 +4,17 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  const { isAuthenticated } = useSelector((state) => state.user);
+  const { isAuthenticated, isSeller, user, loading } = useSelector((state) => state.user);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (isAuthenticated) {
       navigate("/");
     }
-  }, []);
+    if(isSeller) {
+      navigate(`/seller/${user._id}`);
+    }
+  }, [loading, isSeller]);
 
   return (
     <div>
