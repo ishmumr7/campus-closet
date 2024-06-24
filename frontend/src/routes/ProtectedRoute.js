f01aa4b5e90df3432094b1978dbd08ebdf79b8ff
Ishmum <ishmum20000@gmail.com>
2024-06-24 20:53:45 +0800

    Add loading

--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -1,9 +1,12 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import Loader from "../components/Layout/Loader";
 
 const ProtectedRoute = ({ children }) => {
   const { loading, isAuthenticated, user } = useSelector((state) => state.user);
-  if (loading === false) {
+  if (loading) {
+    return <Loader />
+  } else {
     if (!isAuthenticated) {
       return <Navigate to="/login" replace />;
     }
