c3d092d6d117b5cea0975ab41d71e6e42b4a43a4
Ishmum <ishmum20000@gmail.com>
2024-06-24 20:50:48 +0800

    Add loading animation

--- frontend/src/routes/SellerProtectedRoute.js
+++ frontend/src/routes/SellerProtectedRoute.js
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import Loader from "../components/Layout/Loader";
 // import Loader from "../components/Layout/Loader";
 
 const SellerProtectedRoute = ({ children }) => {
   const { loading, isSeller } = useSelector((state) => state.user);
 
   if (loading === true) {
-    return <div />;
+    return <Loader />;
   } else {
     if (!isSeller) {
       return <Navigate to={`/`} replace />;
