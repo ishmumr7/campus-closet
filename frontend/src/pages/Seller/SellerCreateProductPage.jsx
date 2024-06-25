5bc93ef7c5330510768db19c3127bfe5d6deae45
Ishmum <ishmum20000@gmail.com>
2024-06-25 20:32:17 +0800

    Update css

--- frontend/src/pages/Seller/SellerCreateProductPage.jsx
+++ frontend/src/pages/Seller/SellerCreateProductPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import DashboardHeader from "../../components/Seller/Layout/DashboardHeader";
 import DashboardSideBar from "../../components/Seller/Layout/DashboardSideBar";
-import CreateProduct from "../../components/Seller/CreateProduct.jsx";
+import CreateProduct from "../../components/Seller/CreateProduct";
 
 const SellerCreateProductPage = () => {
   return (
     <div>
       <DashboardHeader />
-      <div className="flex items-start justify-between w-full">
+      <div className="flex items-center justify-between w-full">
         <div className="w-[80px] 800px:w-[330px]">
           <DashboardSideBar active={4} />
         </div>
