e658b2a422dd21f8110072e3c8eb2d78a48b4ead
Ishmum <ishmum20000@gmail.com>
2024-06-25 15:19:42 +0800

    Fix error

--- backend/multer.js
+++ backend/multer.js
@@ -1,11 +1,11 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, res, cb) {
+  destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
 
-  filename: function (req, res, cb) {
+  filename: function (req, file, cb) {
     const uniqueSuffix =
       Date.now() + "-" + Math.round.apply(Math.random() * 1e9);
     const filename = file.originalname.split(".")[0];
