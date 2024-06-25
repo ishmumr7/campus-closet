07441d727865cb7c6a3cb7dbee9584e6136e13b4
Ishmum <ishmum20000@gmail.com>
2024-06-25 12:38:06 +0800

    Format code

--- frontend/src/pages/Seller/SellerDashboardPage.jsx
+++ frontend/src/pages/Seller/SellerDashboardPage.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import DashboardHeader from './../../components/Seller/Layout/DashboardHeader';
-import DashboardSideBar from './../../components/Seller/Layout/DashboardSideBar.jsx';
+import React from "react";
+import DashboardHeader from "./../../components/Seller/Layout/DashboardHeader";
+import DashboardSideBar from "./../../components/Seller/Layout/DashboardSideBar.jsx";
 
 const SellerDashboardPage = () => {
   return (
     <div>
       <DashboardHeader />
       <div className="flex items-start justify-between w-full">
-            <div className="w-[80px] 800px:w-[330px]">
-              <DashboardSideBar active={1} />
-            </div>
-            {/* <DashboardHero /> */}
-          </div>
+        <div className="w-[80px] 800px:w-[330px]">
+          <DashboardSideBar active={1} />
+        </div>
+        {/* <DashboardHero /> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SellerDashboardPage
+export default SellerDashboardPage;
