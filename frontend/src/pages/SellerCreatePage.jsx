d42607774bc959e5e359d825386e5ba9a59625df
Ishmum <ishmum20000@gmail.com>
2024-06-29 11:04:22 +0800

    Navigate to home if already logged in and seller

--- frontend/src/pages/SellerCreatePage.jsx
+++ frontend/src/pages/SellerCreatePage.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import SellerCreate from '../components/Seller/SellerCreate';
+import React, { useEffect } from "react";
+import SellerCreate from "../components/Seller/SellerCreate";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const SellerCreatePage = () => {
+  const { isSeller } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isSeller) {
+      navigate("/dashboard");
+    }
+  }, [isSeller, navigate]);
+
   return (
     <div>
       <SellerCreate />
     </div>
-  )
-}
+  );
+};
 
 export default SellerCreatePage;
