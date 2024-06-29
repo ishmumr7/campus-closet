2d649f4fdefa2a256ec0e4f0db540e01a4b3f631
Ishmum <ishmum20000@gmail.com>
2024-06-29 23:42:15 +0800

    Add seller coupon get

--- backend/controller/couponCode.js
+++ backend/controller/couponCode.js
@@ -3,28 +3,46 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../model/user");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { isSeller } = require("../middleware/auth");
-const CoupounCode = require("../model/couponCode");
+const CouponCode = require("../model/couponCode");
 const router = express.Router();
 
-// create coupoun code
+// create coupon code
 router.post(
   "/create-coupon-code",
   isSeller,
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const coupounCodeExists = await CoupounCode.find({
+      const couponCodeExists = await CouponCode.find({
         name: req.body.name,
       });
 
-      if (coupounCodeExists.length !== 0) {
-        return next(new ErrorHandler("Coupoun code already exists!", 400));
+      if (couponCodeExists.length !== 0) {
+        return next(new ErrorHandler("Coupon code already exists!", 400));
       }
 
-      const coupounCode = await CoupounCode.create(req.body);
+      const couponCode = await CouponCode.create(req.body);
 
       res.status(201).json({
         success: true,
-        coupounCode,
+        couponCode,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+// Get all coupons seller
+router.get(
+  "/get-coupon/:id",
+  isSeller,
+  catchAsyncErrors(async (req, res) => {
+    try {
+      const couponCode = await CouponCode.find({ id: req.params.id });
+
+      res.status(201).json({
+        success: true,
+        couponCode,
       });
     } catch (error) {
       return next(new ErrorHandler(error, 400));
