908099dd154a36e68fccedf2e6e30beeab872fc2
Ishmum <ishmum20000@gmail.com>
2024-06-29 12:17:24 +0800

    Update var name

--- backend/model/event.js
+++ backend/model/event.js
@@ -17,7 +17,7 @@ const eventSchema = new mongoose.Schema({
         type: Date,
         required: true,
       },
-      Finish_Date: {
+      end_Date: {
         type: Date,
         required: true,
       },
