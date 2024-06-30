40ef205db871bb3d0eefdcdd03e178093846c9e2
Ishmum <ishmum20000@gmail.com>
2024-06-30 13:28:31 +0800

    Integrate with backend

--- frontend/src/components/Seller/SellerInfo.jsx
+++ frontend/src/components/Seller/SellerInfo.jsx
@@ -1,14 +1,33 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import styles from "../../styles/styles";
 import axios from "axios";
 import { server } from "../../server";
 import { toast } from "react-toastify"
+import { getAllProductsSeller } from "../../redux/actions/product";
 
 const SellerInfo = ({ isOwner }) => {
-  const { loading, user } = useSelector((state) => state.user);
+  const [data,setData] = useState({});
+  const {products} = useSelector((state) => state.products);
+  const [isLoading,setIsLoading] = useState(false);
+  const {id} = useParams();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getAllProductsSeller(id));
+    setIsLoading(true);
+    axios.get(`${server}/user/get-user-info/${id}`).then((res) => {
+     setData(res.data.user);
+     setIsLoading(false);
+    }).catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+    })
+  }, [dispatch, id])
+
+  console.log(data);
 
   const logoutHandler = () => {
     axios.get(`${server}/user/logout`, { withCredentials: true }).then((res) => {
@@ -25,21 +44,21 @@ const SellerInfo = ({ isOwner }) => {
       <div className="w-full flex item-center justify-center">
         <img
           src={`https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg`}
-          alt={user.name}
+          alt={data.name}
           className="w-[150px] h-[150px] object-cover rounded-full"
         />
       </div>
-      <h3 className="text-center py-2 text-[20px]">{user.name}</h3>
+      <h3 className="text-center py-2 text-[20px]">{data.name}</h3>
       <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
-        {user.description}
+        {data.description}
       </p>
       <div className="p-3">
         <h5 className="font-[600]">Address</h5>
-        <h4 className="text-[#000000a6]">{user.addresses[0].address1}</h4>
+        <h4 className="text-[#000000a6]">{data.addresses[0].address1}</h4>
       </div>
       <div className="p-3">
         <h5 className="font-[600]">Phone Number</h5>
-        <h4 className="text-[#000000a6]">{user.phoneNumber}</h4>
+        <h4 className="text-[#000000a6]">{data.phoneNumber}</h4>
       </div>
       <div className="p-3">
         <h5 className="font-[600]">Total Products</h5>
@@ -51,7 +70,7 @@ const SellerInfo = ({ isOwner }) => {
       </div>
       <div className="p-3">
         <h5 className="font-[600]">Joined On</h5>
-        <h4 className="text-[#000000b0]">{user?.createdAt?.slice(0, 10)}</h4>
+        <h4 className="text-[#000000b0]">{data?.createdAt?.slice(0, 10)}</h4>
       </div>
       {isOwner && (
         <div className="py-3 px-4">
