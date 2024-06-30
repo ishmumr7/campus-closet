76688a2f0cb89f5c4b26600e0bcfd570488a99e0
Ishmum <ishmum20000@gmail.com>
2024-07-01 02:11:48 +0800

    Add wishlist state

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,6 +3,7 @@ import { userReducer } from "./reducers/user";
 import { productReducer } from "./reducers/product";
 import { eventReducer } from "./reducers/event";
 import { cartReducer } from "./reducers/cart";
+import { wishlistReducer } from "./reducers/wishlist";
 
 const Store = configureStore({
   reducer: {
@@ -10,7 +11,10 @@ const Store = configureStore({
     products: productReducer,
     events: eventReducer,
     cart: cartReducer,
+    wishlist: wishlistReducer,
   },
 });
+
+console.log(Store.getState());
 
 export default Store;
