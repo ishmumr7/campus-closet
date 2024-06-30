96c2a931c8ec123efa5039670be8bbf684f66a65
Ishmum <ishmum20000@gmail.com>
2024-06-30 14:59:26 +0800

    Remove margin for profile data

--- frontend/src/pages/SellerPreviewPage.jsx
+++ frontend/src/pages/SellerPreviewPage.jsx
@@ -10,7 +10,7 @@ const SellerPreviewPage = () => {
           <div className="800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm 800px:overflow-y-scroll 800px:h-[90vh] 800px:sticky top-10 left-0 z-10">
             <SellerInfo isOwner={false} />
           </div>
-          <div className="800px:w-[72%] mt-5 800px:mt-['unset'] rounded-[4px]">
+          <div className="800px:w-[72%] 800px:mt-['unset'] rounded-[4px]">
             <SellerProfileData isOwner={false} />
           </div>
          </div>
