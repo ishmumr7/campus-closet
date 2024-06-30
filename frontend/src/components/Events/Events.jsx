bb4d748072d7d081fda6159aa09192683dfe22bd
Ishmum <ishmum20000@gmail.com>
2024-06-30 19:20:20 +0800

    Integrate with backend

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import EventCard from "./EventCard.jsx";
 import styles from "../../styles/styles.js";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllEvents } from "../../redux/actions/event.js";
 
 const Events = () => {
+  const { allEvents, isLoading } = useSelector((state) => state.events);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(getAllEvents);
+  // }, [dispatch])
+
+  console.log(allEvents)
   return (
     <div>
-      <div className={`${styles.section}`}>
-        <div className={styles.heading}>Events</div>
+      {!isLoading && (
+        <div className={`${styles.section}`}>
+          <div className={`${styles.heading}`}>
+            <h1>Popular Events</h1>
+          </div>
 
-        <div className="w-full grid">
-          <EventCard />
+          <div className="w-full grid">
+            {allEvents.length !== 0 && <EventCard data={allEvents && allEvents[0]} />}
+            <h4>{allEvents?.length === 0 && "No Events have!"}</h4>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
