bd49d84a93f1eed66cf8c6b04675b5ee9783cd17
Ishmum <ishmum20000@gmail.com>
2024-06-30 14:38:29 +0800

    Integrate with backend

--- frontend/src/components/Seller/SellerInfo.jsx
+++ frontend/src/components/Seller/SellerInfo.jsx
@@ -4,89 +4,109 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import styles from "../../styles/styles";
 import axios from "axios";
 import { server } from "../../server";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 import { getAllProductsSeller } from "../../redux/actions/product";
+import Loader from "../Layout/Loader";
 
 const SellerInfo = ({ isOwner }) => {
-  const [data,setData] = useState({});
-  const {products} = useSelector((state) => state.products);
-  const [isLoading,setIsLoading] = useState(false);
-  const {id} = useParams();
+  const [data, setData] = useState({});
+  const { products } = useSelector((state) => state.products);
+  const [isLoading, setIsLoading] = useState(false);
+  const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getAllProductsSeller(id));
     setIsLoading(true);
-    axios.get(`${server}/user/get-user-info/${id}`).then((res) => {
-     setData(res.data.user);
-     setIsLoading(false);
-    }).catch((error) => {
-      console.log(error);
-      setIsLoading(false);
-    })
-  }, [dispatch, id])
-
-  console.log(data);
+    axios
+      .get(`${server}/user/get-user-info/${id}`)
+      .then((res) => {
+        setData(res.data.user);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, [dispatch, id]);
 
   const logoutHandler = () => {
-    axios.get(`${server}/user/logout`, { withCredentials: true }).then((res) => {
-      window.location.reload();
-      navigate("/");
-      toast.success(res.data.message);
-    }).catch((error) => {
-      console.log(error.response.data.message)
-    })
-  }
+    axios
+      .get(`${server}/user/logout`, { withCredentials: true })
+      .then((res) => {
+        window.location.reload();
+        navigate("/");
+        toast.success(res.data.message);
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
+  };
 
   return (
-    <div className="w-full py-5">
-      <div className="w-full flex item-center justify-center">
-        <img
-          src={`https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg`}
-          alt={data.name}
-          className="w-[150px] h-[150px] object-cover rounded-full"
-        />
-      </div>
-      <h3 className="text-center py-2 text-[20px]">{data.name}</h3>
-      <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
-        {data.description}
-      </p>
-      <div className="p-3">
-        <h5 className="font-[600]">Address</h5>
-        <h4 className="text-[#000000a6]">{data.addresses[0].address1}</h4>
-      </div>
-      <div className="p-3">
-        <h5 className="font-[600]">Phone Number</h5>
-        <h4 className="text-[#000000a6]">{data.phoneNumber}</h4>
-      </div>
-      <div className="p-3">
-        <h5 className="font-[600]">Total Products</h5>
-        <h4 className="text-[#000000a6]">{`3`}</h4> {/* Add dynamic */}
-      </div>
-      <div className="p-3">
-        <h5 className="font-[600]">Seller Ratings</h5>
-        <h4 className="text-[#000000b0]">{`4.3`}/5</h4> {/* Add dynamic */}
-      </div>
-      <div className="p-3">
-        <h5 className="font-[600]">Joined On</h5>
-        <h4 className="text-[#000000b0]">{data?.createdAt?.slice(0, 10)}</h4>
-      </div>
-      {isOwner && (
-        <div className="py-3 px-4">
-           <Link to="/settings">
-           <div className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}>
-            <span className="text-white">Edit Info</span>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full py-5">
+          <div className="w-full flex item-center justify-center">
+            <img
+              src={`https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg`}
+              alt={data.name}
+              className="w-[150px] h-[150px] object-cover rounded-full"
+            />
           </div>
-           </Link>
-          <div className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
-          onClick={logoutHandler}
-          >
-            <span className="text-white">Log Out</span>
+          <h3 className="text-center py-2 text-[20px]">{data.name}</h3>
+          <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
+            {data.description}
+          </p>
+          <div className="p-3">
+            <h5 className="font-[600]">Address</h5>
+            <h4 className="text-[#000000a6]">
+              {
+                (data?.addresses?.length > 0) ? data.addresses[0].address1 : ""
+              }
+            </h4>
           </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Phone Number</h5>
+            <h4 className="text-[#000000a6]">{data.phoneNumber}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Total Products</h5>
+            <h4 className="text-[#000000a6]">{`3`}</h4> {/* Add dynamic */}
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Seller Ratings</h5>
+            <h4 className="text-[#000000b0]">{`4.3`}/5</h4> {/* Add dynamic */}
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Joined On</h5>
+            <h4 className="text-[#000000b0]">
+              {data?.createdAt?.slice(0, 10)}
+            </h4>
+          </div>
+          {isOwner && (
+            <div className="py-3 px-4">
+              <Link to="/settings">
+                <div
+                  className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
+                >
+                  <span className="text-white">Edit Info</span>
+                </div>
+              </Link>
+              <div
+                className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
+                onClick={logoutHandler}
+              >
+                <span className="text-white">Log Out</span>
+              </div>
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/Seller/SellerProfileData.jsx
+++ frontend/src/components/Seller/SellerProfileData.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import styles from "../../styles/styles";
-import { productData } from "../../static/data";
 import ProductCard from "../ProductCard/ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProductsSeller } from "../../redux/actions/product";
+import { getAllEventsSeller } from "../../redux/actions/event";
 
 const SellerProfileData = ({ isOwner }) => {
   const [active, setActive] = useState(1);
+  const { products } = useSelector((state) => state.products);
+  const { events } = useSelector((state) => state.events);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProductsSeller(id));
+    dispatch(getAllEventsSeller(id));
+  }, [dispatch]);
 
   return (
     <div className="w-full">
@@ -55,10 +66,31 @@ const SellerProfileData = ({ isOwner }) => {
       </div>
       {active === 1 && (
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
-          {productData &&
-            productData.map((i, index) => (
+          {products &&
+            products.map((i, index) => (
               <ProductCard data={i} key={index} isShop={true} />
             ))}
+        </div>
+      )}
+
+      {active === 2 && (
+        <div className="w-full">
+          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+            {events &&
+              events.map((i, index) => (
+                <ProductCard
+                  data={i}
+                  key={index}
+                  isShop={true}
+                  isEvent={true}
+                />
+              ))}
+          </div>
+          {events && events.length === 0 && (
+            <h5 className="w-full text-center py-5 text-[18px]">
+              No Events have for this shop!
+            </h5>
+          )}
         </div>
       )}
     </div>
