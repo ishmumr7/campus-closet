e9c97ab39fc2e8dc6c3e58d316d8f64f5082289c
Ishmum <ishmum20000@gmail.com>
2024-07-09 01:58:07 +0800

    Map the events to list all events

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -28,8 +28,8 @@ const EventCard = ({ active, data }) => {
 
 
   return (
-    <div className={`w-full block bg-white rounded-lg ${active ? "unset" : "mb-12"} lg:flex p-2 `}>
-      <div className="w-full lg:w-[50%] m-auto">
+    <div className={`w-full block bg-white rounded-lg ${active ? "unset" : "mb-12"} lg:flex p-2 m-3`}>
+      <div className="w-full lg:w-[25%] m-auto">
         <img src={`${backend_url}/${data.images[0]}`} alt="" />
       </div>
 
--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -4,23 +4,28 @@ import EventCard from "../components/Events/EventCard";
 import { useSelector } from "react-redux";
 import Loader from "../components/Layout/Loader";
 import styles from "../styles/styles";
+import Footer from "../components/Layout/Footer";
 
 const EventsPage = () => {
-  const { allEvents, isLoading } = useSelector((state) => state.events);
-  return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div>
-          <Header activeHeading={4} />
-          <div className={`${styles.section} pt-10 `}>
-            <EventCard active={true} data={allEvents && allEvents[0]} />
-          </div>
-        </div>
-      )}
-    </>
-  );
+	const { allEvents, isLoading } = useSelector((state) => state.events);
+	return (
+		<>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<div>
+					<Header activeHeading={4} />
+					<div className={`${styles.section} pt-10 `}>
+						{allEvents &&
+							allEvents.map((i, index) => (
+								<EventCard active={true} data={i} key={index} />
+							))}
+					</div>
+					<Footer />
+				</div>
+			)}
+		</>
+	);
 };
 
 export default EventsPage;
